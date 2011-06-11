a70a1f684c5cbe1cf454f40b1f7841bfa9651bf4
Nate Lillich <nate@lifewanted.com>
2011-06-10 17:25:37 -0700

    Added tokenizer test two

--- tests/test_tokenizer.cpp
+++ tests/test_tokenizer.cpp
@@ -20,10 +20,12 @@ using namespace cpps;
 using namespace std;
 
 void tokenTest_one( void );
+void tokenTest_two( void );
 
 int main( void )
 {
     tokenTest_one();
+    tokenTest_two();
     return 0;
 }
 
@@ -32,21 +34,52 @@ void tokenTest_one( void )
     string code = "'hello world'";
     Token::List* tokenList = Token::tokenize( code );
 
+    cout << "Basic parse test (" << code << "): ";
+
     if( tokenList->size() < 1 )
         cout << "Error: Too few tokens found. Expected 1, found "
              << tokenList->size() << endl;
 
-    if( tokenList->size() > 1 )
+    else if( tokenList->size() > 1 )
         cout << "Error: Too many tokens found. Expected 1, found "
              << tokenList->size() << endl;
+    else
+    {
+        Token& token = *tokenList->at(0);
+        if( token.getType() != Token::StringLiteral )
+            cout << "Error: Wrong token type found. Expected StringLiteral, found "
+                 << token.getTypeString() << endl;
 
-    Token& token = *tokenList->at(0);
-    if( token.getType() != Token::StringLiteral )
-        cout << "Error: Wrong token type found. Expected StringLiteral, found "
-             << token.getTypeString() << endl;
+        else if( token.getString() != "hello world" )
+            cout << "Error: Wrong token string found. Expected 'hello world', found '"
+                 << token.getString() << "'" << endl;
 
-    if( token.getString() != "hello world" )
-        cout << "Error: Wrong token string found. Expected 'hello world', found '"
-             << token.getString() << "'" << endl;
+        else
+            cout << "Pass!" << endl;
+    }
 }
+
+void tokenTest_two( void )
+{
+    string code = "::;:,||&&?{}()[]==!=>=<==+=-=.=*=/=%=&=^=|=<<=>>=>><<++--->"
+                  "><+-*/%.!~&^|";
+    Token::List* tokenList = Token::tokenize( code );
+
+    cout << "Token extraction order test: ";
+
+    bool error = false;
+    int type = (int)Token::Scope;
+    for( Token::List::iterator it = tokenList->begin();
+         it != tokenList->end();
+         ++it, ++type )
+        if( (*it)->getType() != (Token::Type)type )
+        {
+            cout << "Error: Expected " << Token::getTypeString( (Token::Type)type )
+                 << ", found " << (*it)->getTypeString() << endl;
+            error = true;
+        }
+    if( !error && type == (int)Token::BitwiseOr + 1 )
+        cout << "Pass!" << endl;
+}
+
 
