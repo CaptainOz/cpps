d11fe3f32c8d46f9d8acf7ef2f0e7a84e8a45f07
Nate Lillich <nate@lifewanted.com>
2011-06-29 11:02:32 -0700

    Started adding virtual operator methods.

--- cpps/Scriptable.h
+++ cpps/Scriptable.h
@@ -9,6 +9,8 @@
 #ifndef __CPPS_SCRIPTABLE_H_INCLUDED__
 #define __CPPS_SCRIPTABLE_H_INCLUDED__
 
+#include <iostream>
+
 #include "SmartPointer.h"
 
 namespace cpps
@@ -26,9 +28,57 @@ public:
 
     //! Constructor and destructor do nothing.
     Scriptable( void );
-    ~Scriptable( void );
+    virtual ~Scriptable( void );
+
+protected:
+    virtual Reference oprtrAssign( const Reference& rhs );
+    virtual Reference oprtrAssignBitAnd( const Reference& rhs );
+    virtual Reference oprtrAssignBitNot( const Reference& rhs );
+    virtual Reference oprtrAssignBitOr( const Reference& rhs );
+    virtual Reference oprtrAssignBitXOr( const Reference& rhs );
+    virtual Reference oprtrAssignConcat( const Reference& rhs );
+    virtual Reference oprtrAssignDivide( const Reference& rhs );
+    virtual Reference oprtrAssignLeftShift( const Reference& rhs );
+    virtual Reference oprtrAssignMinus( const Reference& rhs );
+    virtual Reference oprtrAssignModulo( const Reference& rhs );
+    virtual Reference oprtrAssignMultiply( const Reference& rhs );
+    virtual Reference oprtrAssignPlus( const Reference& rhs );
+    virtual Reference oprtrAssignRightShift( const Reference& rhs );
+    virtual Refernece oprtrBitAnd( const Reference& rhs ) const;
+    virtual Reference oprtrBitNot( const Reference& rhs ) const;
+    virtual Reference oprtrBitOr( const Reference& rhs ) const;
+    virtual Reference oprtrBitXOr( const Reference& rhs ) const;
+    virtual Reference oprtrCall( const Reference& args ); // Function call
+    virtual Reference oprtrConcat( const Reference& args ) const;
+    virtual Reference oprtrDivide( const Reference& args ) const;
+    virtual Reference oprtrEquality( const Reference& rhs ) const;
+    virtual Reference oprtrGreaterEqual( const Reference& rhs ) const;
+    virtual Reference oprtrGreaterThan( const Reference& rhs ) const;
+    virtual Reference oprtrIndex( const Reference& index );
+    virtual Reference oprtrInstanceOf( const Reference& rhs ) const;
+    virtual Reference oprtrLeftShift( const Reference& rhs ) const;
+    virtual Reference oprtrLessEqual( const Reference& rhs ) const;
+    virtual Reference oprtrLessThan( const Reference& rhs ) const;
+    virtual Reference oprtrMemberAccess( const Reference &rhs );
+    virtual Reference oprtrMinus( const Reference& rhs ) const;
+    virtual Reference oprtrModulo( const Reference& rhs ) const;
+    virtual Reference oprtrMultiply( const Reference& rhs ) const;
+    virtual Reference oprtrNegate( void ) const; // Unary minus
+    virtual Reference oprtrNotEquality( const Reference& rhs ) const;
+    virtual Reference oprtrPlus( const Reference& rhs ) const;
+    virtual Reference oprtrPostDecrement( void );
+    virtual Reference oprtrPostIncrement( void );
+    virtual Reference oprtrPreDecrement( void );
+    virtual Reference oprtrPreIncrement( void );
+    virtual Reference oprtrRightShift( const Reference& rhs ) const;
 
 }; // end class Scriptable
+
+
+inline std::ostream operator<<( std::ostream& out, const Scriptable& obj )
+{
+    return out << obj.stringify();
+}
 
 
 /******************************************************************************/
