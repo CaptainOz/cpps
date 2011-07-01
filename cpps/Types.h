a97a1877a9e2b8d7f8e9faa6e50c614bfdbd91cf
Nate Lillich <nate@lifewanted.com>
2011-07-01 09:26:00 -0700

    Added header to easilly include all cpps type classes

---
+++ cpps/Types.h
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+
+
+#ifndef __CPPS_TYPES_H_INClUDED__
+#define __CPPS_TYPES_H_INCLUDED__
+
+#include "Scriptable.h"
+#include "Object.h"
+#include "Number.h"
+
+#endif // __CPPS_TYPES_H_INCLUDED__
+
