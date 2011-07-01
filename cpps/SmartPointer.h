b3531a18557d0986ab83dafa630b98a4366bd2ab
Nate Lillich <nate@lifewanted.com>
2011-06-30 23:39:44 -0700

    Massive refactoring on smart pointers to remove need for template specialization.

--- cpps/SmartPointer.h
+++ cpps/SmartPointer.h
@@ -14,28 +14,70 @@
 namespace cpps
 {
 
+template< typename DataType, typename RefCounterType = unsigned int >
+class SmartPointerData_base
+{
+public:
+    SmartPointerData_base( void ) {};
+    virtual ~SmartPointerData_base( void ) {};
+
+    virtual const DataType& getData( void ) const = 0;
+    virtual DataType& getData( void ) = 0;
+    
+    virtual const RefCounterType& getCounter( void ) const = 0;
+    virtual RefCounterType& getCounter( void ) = 0;
+}; // end class SmartPointerData_base
+
+
+template< typename DataType, typename RefCounterType = unsigned int >
+class SmartPointerData : public SmartPointerData_base< DataType, RefCounterType >
+{
+private:
+    typedef SmartPointerData< DataType, RefCounterType > ThisType;
+
+    RefCounterType m_refCount;
+    DataType* m_dataPtr;
+
+public:
+    SmartPointerData( void );
+    SmartPointerData( const ThisType& other );
+    SmartPointerData( const DataType& data );
+    SmartPointerData( DataType* dataPtr );
+    
+    virtual ~SmartPointerData( void );
+    
+    virtual const DataType& getData( void ) const;
+    virtual DataType& getData( void );
+
+    virtual const RefCounterType& getCounter( void ) const;
+    virtual RefCounterType& getCounter( void );
+}; // end class SmartPointerData
+
 
 //! A more advanced garbage collection class than auto_ptr.
 /**
  * SmartPointers keep a count of the number of references to themselves and will
  * automatically free their memory once the number of references drops to zero.
  *
- * @tparam DataType    The type of data that the SmartPointer will point to.
- * @tparam CounterType The type to use to keep count of the number of references
- *                     to the SmartPointer. This type must support the
- *                     pre-increment, pre-decrement, bool conversion, and
- *                     unsigned int conversion operators.
+ * @tparam DataType       The type of data that the SmartPointer will point to.
+ * @tparam RefCounterType The type to use to keep count of the number of
+ *                        references to the SmartPointer. This type must support
+ *                        the pre-increment, pre-decrement, bool conversion, and
+ *                        unsigned int conversion operators.
+ * @tparam ContainerType  The type to contain the data and reference counter
+ *                        objects. This type should inherit from the template
+ *                        class SmartPointerData_base.
  */
-template< typename DataType, typename CounterType = unsigned int >
+template<
+        typename DataType,
+        typename RefCounterType = unsigned int,
+        typename ContainerType = SmartPointerData< DataType, RefCounterType >
+    >
 class SmartPointer
 {
 private:
-    typedef SmartPointer< DataType, CounterType > ThisType;
-    CounterType* m_refCount;
-    DataType* m_dataPtr;
-
-    //! Copy constructor used by SmartPointer::cast.
-    SmartPointer( DataType* dataPtr, CounterType* counter ) throw();
+    typedef SmartPointer< DataType, RefCounterType, ContainerType > ThisType;
+    ContainerType* m_data;
 
     //! Decrements our reference counter and frees the data if we have dropped
     //! to zero references.
@@ -53,14 +95,6 @@ private:
      */
     void _copy( const ThisType& other ) throw();
 
-    //! Returns a reference to the reference counter pointer.
-    CounterType* const& _getCounter( void ) const throw();
-    CounterType*& _getCounter( void ) throw();
-
-    //! Returns a reference to the data pointer.
-    DataType* const& _getDataPtr( void ) const throw();
-    DataType*& _getDataPtr( void ) throw();
-
     void _destroy( void ) throw();
 
 public:
@@ -69,6 +103,9 @@ public:
 
     //! Copy constructor
     /**
+     * Both SmartPointers will be pointing to the same instance of the data and
+     * the reference counter will be incremented.
+     *
      * @param other The other SmartPointer to copy.
      */
     SmartPointer( const ThisType& other ) throw();
@@ -103,10 +140,6 @@ public:
      */
     ThisType clone( void ) const throw();
 
-    //! Casts this smart pointer to a pointer of another type.
-    template< typename CastType >
-    SmartPointer< CastType, CounterType > castTo( void ) throw();
-
     //! Copy operator
     /**
      * @param other The other SmartPointer to copy.
@@ -134,52 +167,38 @@ public:
 /******************************************************************************/
 
 
-template< typename D, typename C >
-inline SmartPointer<D,C>::SmartPointer( void ) throw()
-    : m_refCount( NULL ),
-      m_dataPtr( NULL )
+template< typename D, typename R, typename C >
+inline SmartPointer<D,R,C>::SmartPointer( void ) throw()
+    : m_data( NULL )
 {
 }
 
-template< typename D, typename C >
-SmartPointer<D,C>::SmartPointer( const ThisType& other ) throw()
-    : m_refCount( NULL ),
-      m_dataPtr( NULL )
+template< typename D, typename R, typename C >
+SmartPointer<D,R,C>::SmartPointer( const ThisType& other ) throw()
+    : m_data( NULL )
 {
     _copy( other );
     _increment();
 }
 
-template< typename D, typename C >
-SmartPointer<D,C>::SmartPointer( D* dataPtr ) throw()
-    : m_refCount( new C(0) ),
-      m_dataPtr( dataPtr )
+template< typename D, typename R, typename C >
+SmartPointer<D,R,C>::SmartPointer( D* dataPtr ) throw()
+    : m_data( new C(dataPtr) )
 {
-    _increment();
 }
 
-template< typename D, typename C >
-SmartPointer<D,C>::SmartPointer( const D& data ) throw()
-    : m_refCount( new C(0) ),
-      m_dataPtr( new D(data) )
+template< typename D, typename R, typename C >
+SmartPointer<D,R,C>::SmartPointer( const D& data ) throw()
+    : m_data( new C(data) )
 {
-    _increment();
-}
-
-template< typename D, typename C >
-SmartPointer<D,C>::SmartPointer( D* dataPtr, C* counter ) throw()
-    : m_refCount( counter ),
-      m_dataPtr( dataPtr )
-{
-    _increment();
 }
 
 
 /******************************************************************************/
 
 
-template< typename D, typename C >
-inline SmartPointer<D,C>::~SmartPointer( void ) throw()
+template< typename D, typename R, typename C >
+inline SmartPointer<D,R,C>::~SmartPointer( void ) throw()
 {
     _decrement();
 }
@@ -188,11 +207,10 @@ inline SmartPointer<D,C>::~SmartPointer( void ) throw()
 /******************************************************************************/
 
 
-template< typename D, typename C >
-void SmartPointer<D,C>::_decrement( void ) throw()
+template< typename D, typename R, typename C >
+void SmartPointer<D,R,C>::_decrement( void ) throw()
 {
-    C*& refCount = _getCounter();
-    if( refCount != NULL && --(*refCount) <= 0 )
+    if( m_data != NULL && --(m_data->getCounter()) <= 0 )
         _destroy();
 }
 
@@ -200,110 +218,60 @@ void SmartPointer<D,C>::_decrement( void ) throw()
 /******************************************************************************/
 
 
-template< typename D, typename C >
-inline void SmartPointer<D,C>::_increment( void ) throw()
+template< typename D, typename R, typename C >
+inline void SmartPointer<D,R,C>::_increment( void ) throw()
 {
-    C*& refCount = _getCounter();
-    if( refCount != NULL )
-        ++(*refCount);
+    if( m_data != NULL )
+        ++(m_data->getCounter());
 }
 
 
 /******************************************************************************/
 
 
-template< typename D, typename C >
-void SmartPointer<D,C>::_copy( const ThisType& other ) throw()
+template< typename D, typename R, typename C >
+inline void SmartPointer<D,R,C>::_copy( const ThisType& other ) throw()
 {
-    ThisType* otherPtr = const_cast<ThisType*>( &other );
-    _getCounter() = otherPtr->_getCounter();
-    _getDataPtr() = otherPtr->_getDataPtr();
+    m_data = (const_cast<ThisType*>( &other ))->m_data;
 }
 
 
 /******************************************************************************/
 
 
-template< typename D, typename C >
-inline C* const& SmartPointer<D,C>::_getCounter( void ) const throw()
+template< typename D, typename R, typename C >
+void SmartPointer<D,R,C>::_destroy( void ) throw()
 {
-    return m_refCount;
-}
-
-template< typename D, typename C >
-inline C*& SmartPointer<D,C>::_getCounter( void ) throw()
-{
-    return m_refCount;
+    delete m_data;
+    m_data = NULL;
 }
 
 
 /******************************************************************************/
 
 
-template< typename D, typename C >
-inline D* const& SmartPointer<D,C>::_getDataPtr( void ) const throw()
+template< typename D, typename R, typename C >
+inline unsigned int SmartPointer<D,R,C>::getReferenceCount( void ) const throw()
 {
-    return m_dataPtr;
-}
-
-template< typename D, typename C >
-inline D*& SmartPointer<D,C>::_getDataPtr( void ) throw()
-{
-    return m_dataPtr;
+    return m_data == NULL ? 0 : m_data->getCounter();
 }
 
 
 /******************************************************************************/
 
 
-template< typename D, typename C >
-void SmartPointer<D,C>::_destroy( void ) throw()
+template< typename D, typename R, typename C >
+inline SmartPointer<D,R,C> SmartPointer<D,R,C>::clone( void ) const throw()
 {
-    delete m_refCount;
-    delete m_dataPtr;
-    m_refCount = NULL;
-    m_dataPtr = NULL;
+    return m_data == NULL ? ThisType() : ThisType( m_data->getData() );
 }
 
 
 /******************************************************************************/
 
 
-template< typename D, typename C >
-inline unsigned int SmartPointer<D,C>::getReferenceCount( void ) const throw()
-{
-    C* const& refCount = _getCounter();
-    return refCount != NULL ? *refCount : 0;
-}
-
-
-/******************************************************************************/
-
-
-template< typename D, typename C >
-inline SmartPointer<D,C> SmartPointer<D,C>::clone( void ) const throw()
-{
-    return ThisType( *_getDataPtr() );
-}
-
-
-/******************************************************************************/
-
-
-template< typename D, typename C >
-template< typename CastType >
-inline SmartPointer< CastType, C > SmartPointer<D,C>::castTo( void ) throw()
-{
-    CastType* newDataPtr = dynamic_cast<CastType*>(_getDataPtr());
-    return SmartPointer< CastType, C >( newDataPtr, _getCounter() );
-}
-
-
-/******************************************************************************/
-
-
-template< typename D, typename C >
-SmartPointer<D,C>& SmartPointer<D,C>::operator=( const ThisType& other ) throw()
+template< typename D, typename R, typename C >
+SmartPointer<D,R,C>& SmartPointer<D,R,C>::operator=( const ThisType& other ) throw()
 {
     _decrement();
     _copy( other );
@@ -315,16 +283,32 @@ SmartPointer<D,C>& SmartPointer<D,C>::operator=( const ThisType& other ) throw()
 /******************************************************************************/
 
 
-template< typename D, typename C >
-inline const D& SmartPointer<D,C>::operator*( void ) const throw()
+template< typename D, typename R, typename C >
+inline const D& SmartPointer<D,R,C>::operator*( void ) const throw()
 {
-    return *_getDataPtr();
+    return m_data->getData();
 }
 
-template< typename D, typename C >
-inline D& SmartPointer<D,C>::operator*( void ) throw()
+template< typename D, typename R, typename C >
+inline D& SmartPointer<D,R,C>::operator*( void ) throw()
 {
-    return *_getDataPtr();
+    return m_data->getData();
+}
+
+
+/******************************************************************************/
+
+
+template< typename D, typename R, typename C >
+inline const D* SmartPointer<D,R,C>::operator->( void ) const throw()
+{
+    return &(m_data->getData());
+}
+
+template< typename D, typename R, typename C >
+inline D* SmartPointer<D,R,C>::operator->( void ) throw()
+{
+    return &(m_data->getData());
 }
 
 
@@ -332,15 +316,74 @@ inline D& SmartPointer<D,C>::operator*( void ) throw()
 
 
 template< typename D, typename C >
-inline const D* SmartPointer<D,C>::operator->( void ) const throw()
+SmartPointerData<D,C>::SmartPointerData( void )
+    : m_refCount( 0 ),
+      m_dataPtr( NULL )
 {
-    return _getDataPtr();
 }
 
 template< typename D, typename C >
-inline D* SmartPointer<D,C>::operator->( void ) throw()
+SmartPointerData<D,C>::SmartPointerData( const ThisType& other )
+    : m_refCount( other.m_refCount ),
+      m_dataPtr( new D( *(other.m_dataPtr) ) )
 {
-    return _getDataPtr();
+}
+
+template< typename D, typename C >
+SmartPointerData<D,C>::SmartPointerData( const D& data )
+    : m_refCount( 1 ),
+      m_dataPtr( new D( data ) )
+{
+}
+
+template< typename D, typename C >
+SmartPointerData<D,C>::SmartPointerData( D* dataPtr )
+    : m_refCount( 1 ),
+      m_dataPtr( dataPtr )
+{
+}
+
+
+/******************************************************************************/
+
+
+template< typename D, typename C >
+SmartPointerData<D,C>::~SmartPointerData( void )
+{
+    if( m_dataPtr != NULL )
+        delete m_dataPtr;
+}
+
+
+/******************************************************************************/
+
+
+template< typename D, typename C >
+inline const D& SmartPointerData<D,C>::getData( void ) const
+{
+    return *m_dataPtr;
+}
+
+template< typename D, typename C >
+inline D& SmartPointerData<D,C>::getData( void )
+{
+    return *m_dataPtr;
+}
+
+
+/******************************************************************************/
+
+
+template< typename D, typename C >
+inline const C& SmartPointerData<D,C>::getCounter( void ) const
+{
+    return m_refCount;
+}
+
+template< typename D, typename C >
+inline C& SmartPointerData<D,C>::getCounter( void )
+{
+    return m_refCount;
 }
 
 
